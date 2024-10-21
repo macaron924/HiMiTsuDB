dab45aaa5be466c7fe1d067c4e070913d0697ec9
macaron924 <akarin915mor@gmail.com>
2024-10-21 10:12:47 +0900

    コーデごとに表示、スタイル変更等

--- item/script.js
+++ item/script.js
@@ -1,3 +1,4 @@
+const PART_LIST = ["one-piece", "tops", "bottoms", "shoes", "accessory"]
 let categoryList = [];
 let activeSelections = [];
 
@@ -8,6 +9,209 @@ function isObject(o) {
     return (o instanceof Object && !(o instanceof Array)) ? true : false;
 };
 
+Promise.all([
+    fetch("./../data/item_data.json"),
+    fetch("./../data/coordinate_data.json")
+])
+    .then((results) => Promise.all([results[0].json(), results[1].json()]))
+    .then((res) => {
+        let itemRes = res[0];
+        let coordinateRes = res[1];
+        coordinateRes.forEach((obj) => {
+            const div = document.createElement("div");
+            div.className = "item";
+
+            let category1 = obj.category1;
+            let category2 = obj.category2;
+            div.classList.add(`category-${category1}`);
+            if(!categoryList.includes(category1)) categoryList.push(category1);
+
+            const brandDiv = document.createElement("div");
+            brandDiv.className = "brandDiv";
+            brandDiv.innerText = obj.brandName;
+            div.appendChild(brandDiv);
+
+            const nameDiv = document.createElement("div");
+            nameDiv.innerText = obj.coordinateName;
+            div.appendChild(nameDiv);
+
+            let category1str = "";
+            switch (category1) {
+                case "special":
+                    category1str = "スペシャル";
+                    break;
+                case "gumi":
+                    category1str = "アイプリカード♪コレクショングミ";
+                    break;
+                case "millefeui":
+                    category1str = "ミルフィーカード";
+                    break;
+                default:
+                    category1str = `${category1}だん`;
+            }
+            const categoryDiv = document.createElement("div");
+            categoryDiv.className = "categoryDiv";
+            categoryDiv.innerText = `${category1str} / ${category2}`;
+            div.appendChild(categoryDiv);
+
+            PART_LIST.forEach((part) => {
+                const itemIdArray = obj[part].split(" ");
+                const itemID = itemIdArray[0];
+                let reStr = "";
+                if (itemIdArray.length > 1) reStr = " re";
+                if (itemID == "") return;
+    
+                const numBoxDiv = document.createElement("div");
+                numBoxDiv.className = `numBoxDiv part-${part}${reStr}`;
+
+                const partIconDiv = document.createElement("div");
+                partIconDiv.className = "partIconDiv";
+                div.appendChild(partIconDiv);
+
+                const decrementButton = document.createElement("button");
+                decrementButton.className = "decrement";
+                decrementButton.innerText = "-";
+                decrementButton.addEventListener("click", function() {
+                    const inp = this.parentElement.querySelector("input");
+                    const value = inp.value;
+    
+                    const newValue = value > 0 ? parseInt(value) - 1 : 0;
+                    inp.value = newValue;
+    
+                    const id = inp.getAttribute("itemID");
+                    if (newValue == 0){
+                        delete haveList[id];
+                        inp.classList.remove("have");
+                    }else {
+                        haveList[id] = newValue.toString();
+                        inp.classList.add("have");
+                    }
+                    localStorage.setItem("itemHaveList", JSON.stringify(haveList));
+                })
+
+                const incrementButton = document.createElement("button");
+                incrementButton.className = "increment";
+                incrementButton.innerText = "+";
+                incrementButton.addEventListener("click", function() {
+                    const inp = this.parentElement.querySelector("input");
+                    const value = inp.value;
+    
+                    const newValue = value == "" ? 1 : parseInt(value) + 1;
+                    inp.value = newValue;
+    
+                    const id = inp.getAttribute("itemID");
+                    if (newValue == 0){
+                        delete haveList[id];
+                        inp.classList.remove("have");
+                    }else {
+                        haveList[id] = newValue.toString();
+                        inp.classList.add("have");
+                    }
+                    localStorage.setItem("itemHaveList", JSON.stringify(haveList));
+                })
+
+                const numBox = document.createElement("input");
+                numBox.type = "number";
+                numBox.setAttribute("itemID", itemID)
+                if (haveList[numBox.getAttribute("itemID")] != undefined) {
+                    numBox.value = haveList[numBox.getAttribute("itemID")];
+                    numBox.classList.add("have");
+                }
+                
+                numBoxDiv.appendChild(partIconDiv);
+                numBoxDiv.appendChild(decrementButton);
+                numBoxDiv.appendChild(numBox);
+                numBoxDiv.appendChild(incrementButton);
+                div.appendChild(numBoxDiv);
+            })
+
+            //div.innerText = `${obj.brandName} / ${obj.coordinateName} / ${obj.part}`;
+            document.getElementById("content").appendChild(div);
+        })
+        
+        categoryList.sort();
+        categoryList.forEach((category) => {
+            const categoryButton = document.createElement("button");
+            categoryButton.value = category;
+            let category1str = "";
+            switch (category) {
+                case "special":
+                    category1str = "スペシャル";
+                    break;
+                case "gumi":
+                    category1str = "アイプリカード♪コレクショングミ";
+                    break;
+                case "millefeui":
+                    category1str = "ミルフィーカード";
+                    break;
+                default:
+                    category1str = `${category}だん`;
+            }
+            categoryButton.innerHTML = category1str;
+            categoryButton.addEventListener("click", function() {
+                let value = this.value;
+                if (activeSelections.length == 0) {
+                    document.querySelectorAll(".item").forEach(element => {
+                        element.classList.add("invisible");
+                    })
+                }
+                if(this.classList.contains("active")){
+                    this.classList.remove("active");
+                    activeSelections = activeSelections.filter(i => (i !== value));
+                    document.querySelectorAll(`.item.category-${value}`).forEach(element => {
+                        element.classList.add("invisible");
+                    })
+                }
+                else {
+                    this.classList.add("active");
+                    activeSelections.push(value);
+                    document.querySelectorAll(`.item.category-${value}`).forEach(element => {
+                        element.classList.remove("invisible");
+                    })
+                }
+                if (activeSelections.length == 0) {
+                    document.querySelectorAll(".item").forEach(element => {
+                        element.classList.remove("invisible");
+                    })
+                }
+            })
+            document.getElementById("category-select").appendChild(categoryButton);
+        });
+
+        document.getElementById("storageImport").addEventListener("click", function() {
+            let inputText = document.getElementById("dataInput").value;
+            try {
+                if(isObject(JSON.parse(inputText)) === false) throw "IsNotValidObjectError";
+            } catch(error) {
+                this.innerText = "インポート失敗。";
+                setTimeout(() => {
+                    this.innerText = "インポート";
+                }, 1000);
+                return -1;
+            }
+            haveList = JSON.parse(inputText);
+            localStorage.setItem("itemHaveList", JSON.stringify(haveList));
+            this.innerText = "インポート完了。1秒後にリロードします...";
+            setTimeout(() => {
+                window.location.href = window.location.href;
+            }, 1000);
+        })
+
+        document.getElementById("storageExport").addEventListener("click", function() {
+            let data = haveList;
+            document.getElementById("dataInput").value = JSON.stringify(data);
+        })
+
+        document.getElementById("storageReset").addEventListener("click", function() {
+            localStorage.removeItem("itemHaveList");
+            this.innerText = "保存リセット完了。1秒後にリロードします...";
+            setTimeout(() => {
+                window.location.href = window.location.href;
+            }, 1000);
+        })
+    })
+
+/*
 fetch("./../data/item_data.json")
     .then((r) => r.json())
     .then((res) => {
@@ -221,3 +425,4 @@ fetch("./../data/item_data.json")
             }, 1000);
         })
     })
+*/
